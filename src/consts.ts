03c067bcd499b669baed24876105168518f7096b
Mateo Elias <oscarmateoelias@gmail.com>
2025-01-25 17:04:39 -0600

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'ConfirMaterial';
+export const SITE_DESCRIPTION = 'ConfirMaterial es una web diseñada para los jovenes de catequesis de confirmación, en esta web podran encontrar los temas y materiales dados para la clase.';
